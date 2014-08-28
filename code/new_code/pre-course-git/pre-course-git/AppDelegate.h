a21c82ed242681e56226e0331aeced6004c9026f
Sergio Perez <spinfinite07@Sergios-MacBook-Air.local>
2014-08-28 11:51:56 -0600

    Adds a label to the main view

---
+++ code/new_code/pre-course-git/pre-course-git/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  pre-course-git
+//
+//  Created by Sergio Perez on 8/19/14.
+//  Copyright (c) 2014 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ code/new_code/pre-course-git/pre-course-git/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  pre-course-git
+//
+//  Created by Sergio Perez on 8/19/14.
+//  Copyright (c) 2014 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
