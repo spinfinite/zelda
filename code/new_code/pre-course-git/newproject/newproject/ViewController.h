37ffd588fcf94899f0aa776bd12457125256b6d5
Sergio Perez <spinfinite07@Sergios-MacBook-Air.local>
2014-09-01 18:04:28 -0600

    Add log Hello World

---
+++ code/new_code/pre-course-git/newproject/newproject/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  newproject
+//
+//  Created by Sergio Perez on 9/1/14.
+//  Copyright (c) 2014 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
